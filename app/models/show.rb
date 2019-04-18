0ca21f170d54bd61be4193d58ec5e1f616ab8123
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-04-18 19:46:13 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,13 +1,13 @@
 class Show < ActiveRecord::Base
   def self.highest_rating
-    Show.maximum(:rating)
+    self.maximum(:rating)
   end
 
   def self.most_popular_show
     all.order(:rating).last
   end
   def self.lowest_rating
-    Show.minimum(:rating)
+    self.minimum(:rating)
   end
 
   def self.least_popular_show
@@ -15,15 +15,15 @@ class Show < ActiveRecord::Base
   end
 
   def self.ratings_sum
-    Show.sum(:rating)
+    self.sum(:rating)
   end
 
   def self.popular_shows
-    Show.where("rating >= ?", 5)
+    self.where("rating >= ?", 5)
   end
 
   def self.shows_by_alphabetical_order
-
+    self.order(:name)
   end
 
 end
